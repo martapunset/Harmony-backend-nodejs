c7a3284ac38ff8858be72e6e107eda778b6065f7
tonymg1 <tonymartingaldeano@gmail.com>
2023-01-23 09:37:20 +0100

    wip

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,5 @@
 const userRouter = require('express').Router()
 const userController = require('../controllers/user.controller')
-const { jwtCheck } = require('../middlewares/jwt-middleware')
-
-// userRouter
- 
-//   .get('/public', userController.publicPing)
-//   .get('/private',jwtCheck, userController.privatePing)
-//   //.get('/private', userController.privatePing)
-
-// module.exports =  userRouter 
- //.post('/', userController.createUser)
 
 userRouter
     .post("/segundo", userController.createUser)
