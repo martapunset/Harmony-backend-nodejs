89749f159feefafc09fab854a092e5de309d95cf
Lokesh Pereiro <lpdraft001@gmail.com>
2023-02-01 18:59:50 +0100

    wip

--- src/controllers/track.controller.js
+++ src/controllers/track.controller.js
@@ -1,12 +1,12 @@
 const tracksmodel = require("../models/track.model");
 
 const getTracks = async (req, res, next) => {
- // let query = req.query.q;
+  // let query = req.query.q;
 
   try {
-    const allTracks = await tracksmodel.find({})
+    const allTracks = await tracksmodel.find({});
 
-    res.status(200).send({ status: true, data: allTracks});
+    res.status(200).send({ status: true, data: allTracks });
     //next()
   } catch (error) {
     req.status(500).send({ status: false, msg: error.message });
@@ -15,27 +15,24 @@ const getTracks = async (req, res, next) => {
   //next()
 };
 
-
 const searchTracks = async (req, res, next) => {
-const {title} = req.query;
-//const { q } = req.params;
+  const { title } = req.query;
+  //const { q } = req.params;
   try {
-    const findTracks = await tracksmodel.find({ title: title}).lean().exec();
-      //Do your action here..
-  // res.status(200).send({ msg: name });
-  
-   res.status(200).send({ status: true, data: findTracks});
-      //next()
-    } catch (error) {
-      res.status(500).send({ status: false, msg: error.message });
-      //next()
-    }
+    const findTracks = await tracksmodel.find({ title: title }).lean().exec();
+    //Do your action here..
+    // res.status(200).send({ msg: name });
+
+    res.status(200).send({ status: true, data: findTracks });
     //next()
-  };
+  } catch (error) {
+    res.status(500).send({ status: false, msg: error.message });
+    //next()
+  }
+  //next()
+};
 
-
-
-const deleteTrack = async ( req, res, next) => {
+const deleteTrack = async (req, res, next) => {
   const { id } = req.params;
   try {
     const artist = await tracksmodel.findOneAndDelete({ _id: id });
