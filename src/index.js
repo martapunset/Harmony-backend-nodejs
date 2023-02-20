e5631ae5300949352d400ee344d0e3b2b6c9f75b
Marta Punset <martapunset10@gmail.com>
2023-02-20 12:38:32 +0100

    changed port variable in index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const startServer = async () => {
   try {
     console.log(">>", "Try to connect");
     connect(config.logger.info("MongoDB connected"));
-    app.listen(process.env.PORT, () => {
-      config.logger.info(`Server is running in port ${process.env.PORT}`);
+    app.listen(`0.0.0.0:$PORT`, () => {
+      config.logger.info(`Server is running in port ${`0.0.0.0:$PORT`}`);
     });
   } catch (error) {
     console.log("Someting went wrong, server crashed!");
